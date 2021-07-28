c1531fffc581b4c311eeca0bf19821627ec54f65
Dharmi26 <dharmipatel567@gmail.com>
2021-07-28 21:54:51 +0530

    Setup and added Models

---
+++ server/database/restaurant/index.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const RestaurantSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  city: { type: String, required: true },
+  address: { type: String, required: true },
+  mapLocation: { type: String, required: true },
+  cuisine: [String],
+  restaurantTimings: String,
+  contactNumber: Number,
+  website: Number,
+  popularDishes: [String],
+  averageCost: Number,
+  amenties: [String],
+  menuImages: {
+    tupe: mongoose.Types.ObjectId,
+    ref: "Images",
+  },
+  menu: {
+    tupe: mongoose.Types.ObjectId,
+    ref: "Images",
+  },
+  reviews: [{ type: mongoose.Types.ObjectId, ref: "Reviews0" }],
+  photos: { type: mongoose.Types.ObjectId, ref: "Images" },
+});
+
+const RestaurantModel = mongoose.model("Restaurants", RestaurantSchema);

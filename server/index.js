cfc4204e7ec23dc55fa4255af937ace39ede296e
Dharmi26 <dharmipatel567@gmail.com>
2021-09-19 16:42:58 +0530

    add google auth

--- server/index.js
+++ server/index.js
@@ -1,34 +1,41 @@
-// Importing Env variables
+// Importing Env Variables
 require("dotenv").config();
 
 // Libraries
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import passport from "passport";
 
+// configs
+import googleAuthConfig from "./config/google.config";
 // microservice routes
 import Auth from "./API/Auth";
-
-//Database connection
+// Database connection
 import ConnectDB from "./database/connection";
 
 const zomato = express();
 
 // application middlewares
 zomato.use(express.json());
-zomato.use(express.urlencoded({ extended: false}));
+zomato.use(express.urlencoded({ extended: false }));
 zomato.use(helmet());
 zomato.use(cors());
+zomato.use(passport.initialize());
+zomato.use(passport.session());
 
-// Application Routers
+// passport cofiguration
+googleAuthConfig(passport);
+
+// Application Routes
 zomato.use("/auth", Auth);
 
-
-zomato.get("/", (req, res) => res.json({message: "Setup success"}));
+zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
 
 zomato.listen(4000, () =>
   ConnectDB()
-   .then(() => console.log("Server is Running"))
-   .catch(() =>
-    console.log("Server is Running, but database connection failed!"))
+    .then(() => console.log("Server is running 🚀"))
+    .catch(() =>
+      console.log("Server is running, but database connection failed... ")
+    )
 );

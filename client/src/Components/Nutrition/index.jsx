729b923458a942400f89a09102e970a0b81a89a5
Dharmi26 <dharmipatel567@gmail.com>
2021-09-22 16:27:10 +0530

    fixed redirect

--- client/src/Components/Nutrition/index.jsx
+++ client/src/Components/Nutrition/index.jsx
@@ -14,35 +14,11 @@ const Nutrition = () => {
          <NutritionCarousal />
        </div>
         <div className="flex justify-between flex-wrap ">
-      <NutritionCard
-        bg="red"
-        image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
-      />
-      <NutritionCard
-        bg="red"
-        image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
-      />
-      <NutritionCard
-        bg="red"
-        image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
-      />
-      <NutritionCard
-        bg="red"
-        image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
-      />
-      <NutritionCard
-        bg="red"
-        image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
-      />
-      <NutritionCard
-        bg="red"
-        image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
-      />
-      <NutritionCard
-        bg="red"
-        image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
-      />
-    </div>
+         <NutritionCard
+          bg="red"
+          image="https://dote.zmtcdn.com/prod/data/admin_assets/images/985/1db983abf8a38bdebb42171b5ea7d985_1614756360.png?output-format=webp"
+         />
+       </div>
   </div>
   );
 };

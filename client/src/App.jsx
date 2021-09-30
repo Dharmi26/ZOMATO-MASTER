6775fd5b31bf726e819a3c6795329832f602a8e1
Dharmi26 <dharmipatel567@gmail.com>
2021-09-30 18:45:16 +0530

    adding menu, photos and google auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Redirect, Switch } from "react-router-dom";
 import axios from "axios";
-
 // HOC
 import HomeLayoutHOC from "./HOC/Home.Hoc";
 import RestaurantLayoutHOC from "./HOC/Restaurant.HOC";
@@ -15,6 +14,7 @@ import Menu from "./Page/Restaurant/Menu";
 import Photos from "./Page/Restaurant/Photos";
 import Checkout from "./Page/Checkout";
 import RedirectRestaurant from "./Page/Restaurant/Redirect";
+import GoogleAuth from "./Page/GoogleAuth";
 
 // axios global settings
 if (localStorage.zomatoUser) {
@@ -25,41 +25,47 @@ if (localStorage.zomatoUser) {
 function App() {
   return (
     <>
-    <Route path="/" exact>
-      <Redirect to="/delivery" />
-    </Route>
-    <Route path="/restaurant/:id" exact component={RedirectRestaurant} />
+      <Route path="/" exact>
+        <Redirect to="/delivery" />
+      </Route>
+      <Route path="/restaurant/:id" exact component={RedirectRestaurant} />
 
-    <HomeLayoutHOC path="/:type" exact component={Home} />
+      <HomeLayoutHOC path="/:type" exact component={Home} />
 
-    <RestaurantLayoutHOC
-       path="/restaurant/:id/overview"
-       exact
-       component={Overview}
-     />
-     <RestaurantLayoutHOC
-       path="/restaurant/:id/order-online"
-       exact
-       component={OrderOnline}
-     />
-     <RestaurantLayoutHOC
-      path="/restaurant/:id/menu"
-      exact
-      component={Menu}
-     />
-     <RestaurantLayoutHOC
-       path="/restaurant/:id/reviews"
-       exact
-       component={Reviews}
-     />
-     <RestaurantLayoutHOC
-       path="/restaurant/:id/photos"
-       exact
-       component={Photos}
-     />
-     <CheckoutLayoutHOC path="/checkout/orders" exact component={Checkout} />
+      <HomeLayoutHOC path="/google/:token" exact component={GoogleAuth} />
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/overview"
+        exact
+        component={Overview}
+      />
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/order-online"
+        exact
+        component={OrderOnline}
+      />
+      <RestaurantLayoutHOC path="/restaurant/:id/menu" exact component={Menu} />
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/reviews"
+        exact
+        component={Reviews}
+      />
+      <RestaurantLayoutHOC
+        path="/restaurant/:id/photos"
+        exact
+        component={Photos}
+      />
+      <CheckoutLayoutHOC path="/checkout/orders" exact component={Checkout} />
     </>
   );
 }
 
 export default App;
+
+// :type
+
+// delivery
+// dining
+// nightlife
+// nutrition
+
+// master -> type
